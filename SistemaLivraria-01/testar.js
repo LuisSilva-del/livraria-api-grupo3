13c74d9a1e812d00e432a98c243928a011b35e31
Luis Silva <luis.m.silva8@edu.senai.br>
2026-08-26 10:41:58 -0300

    Atualização do projeto 2.0

--- SistemaLivraria-01/testar.js
+++
@@ -1,9 +0,0 @@
-const Cliente = require("./Cliente");
-const Funcionario = require("./Funcionario");
-
-const c1 = new Cliente("Rebeca Souza", "rebeca@email.com", 150);
-const f1 = new Funcionario("Victor Gonçalvez", "victor@livraria.com", 2500);
-
-c1.apresentar();
-console.log("");
-f1.apresentar();
