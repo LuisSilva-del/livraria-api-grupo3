f21c9ea254da45f1a5b20b1986b340ca666a589a
Maria Eduarda da Silva Mendes <maria.e.mendes7@edu.senai.br>
2026-09-04 12:57:30 -0300

    docs: entrega consolidada do Bloco 2 - UML, esqueleto MVC e clean code

---
+++ testar.js
@@ -0,0 +1,10 @@
+const Cliente = require("./Cliente");
+
+const Funcionario = require("./Funcionario");
+
+const c1 = new Cliente("Rebeca Souza", "rebeca@email.com", 150);
+const f1 = new Funcionario("Victor Gonçalves", "victor@livraria.com", 2500);
+
+c1.apresentar();
+console.log("");
+f1.apresentar();
